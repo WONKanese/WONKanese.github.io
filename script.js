67fdadd96f759ae4b80044fc77d69a5671357230
Wonkanese <northkoreanwillywonka@gmail.com>
2024-08-16 10:42:34 -0600

    initialised git

---
+++ script.js
@@ -0,0 +1,25 @@
+for (let i = 1; i < 101; i++) {
+    console.log(i + ". Please forgive me for my horrible css and screen size responsiveness");
+}
+
+document.addEventListener('click', e => {
+    const isDropdownButton = e.target.matches("[data-dropdown-button]")
+
+    if (!isDropdownButton && e.target.closest('[data-dropdown]') != null) 
+    {
+        return;
+    } //if clicking dropdown menu ignore
+
+    let currentDropdown
+    if (isDropdownButton) {
+        currentDropdown = e.target.closest('[data-dropdown]')
+        currentDropdown.classList.toggle('active')
+    }
+
+    document.querySelectorAll("[data-dropdown].active").forEach(dropdown => {
+        if (dropdown === currentDropdown) {
+            return
+        }
+        dropdown.classList.remove('active')
+    })
+})
